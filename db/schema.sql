a01633c8edacb65e2495707be082c4a0b32e5c65
Ws538 <williamsu668@gmail.com>
2023-02-11 19:20:15 +1100

    added view all roles function

--- db/schema.sql
+++ db/schema.sql
@@ -9,16 +9,17 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE roles (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     departments_id INT,
     FOREIGN KEY (departments_id)
     REFERENCES departments(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30), 
     roles_id INT,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,17 @@
 INSERT INTO departments (name)
-VALUES ("William"),
-       ("Karen"),
-       ("Byron"),
-       ("Andrew"),
-       ("Johnson");
+VALUES ("Sales"),
+       ("Engineers"),
+       ("Finance"),
+       ("Legal"),
+       ("Customer Support");
 
-SELECT * FROM departments
+INSERT INTO roles (title, salary, departments_id)
+VALUES ("Sales Lead", 100000, 1),
+       ("Salesperson", 80000, 1),
+       ("Lead Engineer", 180000, 2),
+       ("Software Engineer", 120000, 2),
+       ("Acoount Manager", 150000, 3),
+       ("Accountant", 100000, 3),
+       ("Legal Team Lead", 250000, 4),
+       ("Lawyer", 180000, 4);
+       
