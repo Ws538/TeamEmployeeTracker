e4a1907061478d9cb9fb630cd1adbe071dd1e0e9
Ws538 <williamsu668@gmail.com>
2023-02-11 20:42:48 +1100

    added view all employees query

--- db/query.sql
+++ db/query.sql
@@ -3,16 +3,19 @@ SELECT * FROM departments;
 
 -- view all roles
 SELECT roles.id AS id, 
-roles.title AS name, 
+roles.title AS title, 
 departments.name AS department, 
 roles.salary AS salary
 FROM roles JOIN departments 
 ON roles.departments_id = departments.id;
 
 -- view all Employees
-SELECT roles.id AS id, 
-roles.title AS name, 
+SELECT employee.id AS id,
+employee.first_name AS first_name,
+employee.last_name AS last_name,  
+roles.title AS title, 
 departments.name AS department, 
 roles.salary AS salary
-FROM roles JOIN departments 
-ON roles.departments_id = departments.id;
+FROM employee JOIN roles JOIN departments
+ON employee.roles_id = roles.id 
+WHERE roles.departments_id = departments.id;
